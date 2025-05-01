c22baf6a13522f5c4e745319d98284d6858b7c73
Guna Surya Kumar Katakam <147977592+GunaSuryaKumar@users.noreply.github.com>
2025-05-01 22:15:28 +0530

    Delete server/routes/test-sqlite.js

--- server/routes/test-sqlite.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:', (err) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log('SQLite working');
-    db.close();
-  }
-});
